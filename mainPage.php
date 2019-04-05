aa279f6eee30ddb5ea7b88cc328c289ce02bddd8
Mona Agh <mona.agh1@ucalgary.ca>
2019-04-05 11:23:19 -0600

    main page updated

--- mainPage.php
+++ mainPage.php
@@ -31,48 +31,70 @@
 </div>
 
 <div id="product">
-    <div id="makeup">Make Up</div>
-    <div id="skin">Skincare</div>
-    <div id="fragrance">Fragrance</div>
-    <div id="other">Other</div>
+    <div class="dropdown">
+        <button class="dropbtn">Make Up</button>
+        <div class="dropdown-content">
+            <a href="#">Face</a>
+            <a href="#">Cheek</a>
+            <a href="#">Eye</a>
+            <a href="#">Lip</a>
+            <a href="#">Nail</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Skin Care</button>
+        <div class="dropdown-content">
+            <a href="#">Moisturizers</a>
+            <a href="#">Cleaners</a>
+            <a href="#">Masks</a>
+            <a href="#">Eye Care</a>
+            <a href="#">Sun Care</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Fragrance</button>
+        <div class="dropdown-content">
+            <a href="#">Woman</a>
+            <a href="#">Men</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="dropbtn">Other</button>
+        <div class="dropdown-content">
+            <a href="#">Shampoo & Conditioner</a>
+            <a href="#">Hair Tools</a>
+            <a href="#">Makeup Brushes</a>
+            <a href="#">Accessories</a>
+        </div>
+    </div>
+
 </div>
 
 <div class="page-content">
     <article>
         <span id="popular_heading">Most Popular</span>
-        <Table class="food-table", cellspacing="10">
+        <Table class="product-table", cellspacing="10">
             <tr>
-                <td><a href="focaccia.html"><img src="https://www.homecookingadventure.com/images/recipes/tn2_focaccia_main.jpg" width="180" height="180" alt="Food1"/>
+                <td><a href="#"><img src="https://www.sephora.com/productimages/product/p427421-av-03-zoom.jpg" width="180" height="180"/>
                     </a><br>
-                    <span class="food_name"> Classic Focaccia</span></td>
-                <td><a href="wintercake.html"><img src="https://www.homecookingadventure.com/images/recipes/tn2_winter_cake_main.jpg" width="180" height="180" alt="Food2">
+                    <span> Protini Polypeptide Moisturizer</span></td>
+                <td><a href="#"><img src="https://www.sephora.com/productimages/sku/s870618-main-zoom.jpg" width="180" height="180" alt="Food2">
                     </a><br>
-                    <span class="food_name"> Winter Cake</span></td>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/tn2_buche_de_noel_main.jpg" width="180" height="180" alt="Food3>">
+                    <span> Laura Mercier Translucent Loose Setting Poweder</span></td>
+                <td><img src="https://www.sephora.com/productimages/sku/s2084457-main-zoom.jpg" width="180" height="180" alt="Food3>">
                     <br>
-                    <span class="food_name"> Coconut Buche de Noel</span></td>
+                    <span> Too Faced Concealer</span></td>
             </tr>
             <tr>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/potato-pancakes_main.jpg" width="180px" height="180px" alt="Food4">
+                <td><img src="https://www.sephora.com/productimages/sku/s1637719-main-zoom.jpg"" width="180px" height="180px" alt="Food4">
                     <br>
-                    <span class="food_name"> Cheese Potato Pancakes </span></td>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/roasted-baby-potatoes.jpg" width="180px" height="180px" alt="Food5">
+                    <span> Urban Decay Naked2 Eyeshadow Palette </span></td>
+                <td><img src="https://www.sephora.com/productimages/sku/s1847961-main-zoom.jpg" width="180px" height="180px" alt="Food5">
                     <br>
-                    <span class="food_name"> Roasted Baby Potatoes </span></td>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/raspberry_chocolate_cups_mainimage.jpg" width="180px" height="180px" alt="Food6">
+                    <span> bareMineral Face Powder Foundation </span></td>
+                <td><img src="https://www.sephora.com/productimages/sku/s2093128-main-zoom.jpg" width="180px" height="180px" alt="Food6">
                     <br>
-                    <span class="food_name"> Raspberry Chocolate Cups </span></td>
-            </tr>
-            <tr>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/chicken-lasagna-main-image.jpg" width="180px" height="180px" alt="Food7">
-                    <br>
-                    <span class="food_name"> Chicken Lasagna </span></td>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/Neapolitan_Cake_mainimage.jpg" width="180px" height="180px" alt="Food8">
-                    <br>
-                    <span class="food_name"> Raw Vegan Neapolitan Cake </span></td>
-                <td><img src="https://www.homecookingadventure.com/images/recipes/vanilla-eclairs-main-image.jpg" width="180px" height="180px" alt="Food9">
-                    <br>
-                    <span class="food_name"> Vanilla Eclairs </span></td>
+                    <span> Mac Cosmetics Lipstick </span></td>
             </tr>
         </Table>
     </article>
@@ -83,6 +105,226 @@
         <p id="footer_text">Copyright 2019 INC</div>
     </div>
 </div>
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
 
+    body {
+        background-color: white
+        font-style: Arial;
+
+    }
+
+
+    #header {
+        background-color: black;
+        padding-top:20px;
+        padding-bottom:20px;
+        padding-left: 10px;
+        width: 100%;
+    }
+
+    #banner {
+        padding-top: 10px;
+        padding-bottom: 10px;
+        width:100%;
+        display: flex;
+    }
+
+    #footer {
+        background-color: black;
+        color: #fff;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        padding-left: 10px;
+        width:100%;
+
+    }
+
+    #footer_text {
+        margin: 0 auto;
+    }
+    .container {
+        width: 90%;
+        margin: 0 auto;
+        display:flex;
+    }
+
+    #site_name {
+        color: black;
+        font-size: 50px;
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-transform: uppercase;
+    }
+
+    #icons {
+        width: 25%;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+
+    }
+
+
+    .menu_top {
+        list-style: none;
+        float: right;
+        margin-left:80%;
+
+    }
+
+
+    .menu_top li {
+        display: inline-block;
+        margin-left:30px;
+
+    }
+
+    .menu_top li a {
+        color: white;
+        text-decoration: none;
+    }
+
+    .menu_top li a:hover {
+        text-decoration:underline;
+    }
+
+    .menu_bottom {
+        list-style: none;
+        font-size: 20px;
+        align-items: center;
+        vertical-align: middle;
+        margin: 0 auto;
+    }
+
+
+    .menu_bottom li {
+        display: inline-block;
+        margin-left:30px;
+
+    }
+
+    .menu_bottom li a {
+        color: black;
+        text-decoration: none;
+    }
+
+    .menu_bottom li a:hover {
+        text-decoration:underline;
+    }
+
+
+
+    .page-content {
+        min-height:600px;
+        margin: 0px;
+        padding: 0px;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        flex-flow: row;
+    }
+
+    #popular_heading {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        font-size: 30px;
+    }
+
+    Table.product-table{
+        border: 1px transparent ;
+        width:600px;
+        border-collapse: separate;
+        margin-top:20px;
+
+    }
+
+    #search_container {
+        width: 25%;
+        justify-content: center;
+        align-items: center;
+        display:flex;
+
+    }
+
+
+    #search_container input[type=text] {
+        padding: 6px;
+        margin-top: 8px;
+        font-size: 17px;
+        border: 1px solid lightgrey;
+    }
+
+    #search_container button {
+        position: relative;
+        padding: 6px;
+        margin-top: 8px;
+        margin-right: 16px;
+        background: #ddd;
+        font-size: 17px;
+        border: none;
+        cursor: pointer;
+    }
+
+    #search_container button:hover {
+        background: #ccc;
+    }
+
+    #product {
+        padding-top: 20px;
+        width: 60%;
+        display: flex;
+        margin: 0 auto;
+    }
+
+    .dropdown {
+        width:25%;
+        justify-content: center;
+        align-items: center;
+        display:inline-block;
+        position: relative;
+        padding-left: 7%;
+
+    }
+
+    .dropbtn {
+        background-color: white;
+        color: black;
+        border: none;
+        padding: 16px;
+        font-size: 16px;
+        text-transform: uppercase;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: white;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown-content a:hover {text-decoration: underline}
+
+    .dropdown:hover .dropdown-content {display: block;}
+
+    .dropdown:hover .dropbtn {text-decoration: underline}
+
+
+</style>
 </body>
 </html>
